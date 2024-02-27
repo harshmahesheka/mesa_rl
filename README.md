7e66f4e28271c7ff23add25f1236088c6ab99f0d
mahesheka <harsh.mahesheka.eee20@iitbhu.ac.in>
2024-02-27 23:28:51 +0530

    Seeding the Repository

---
+++ README.md
@@ -0,0 +1,17 @@
+# MESA RL
+
+This repository demonstrates a reinforcement learning (RL) implementation using the Mesa agent framework. It showcases how to solve the Boltzmann wealth model with Proximal Policy Optimization (PPO) from Stable Baselines.
+
+## Key features:
+
+- Boltzmann Wealth Model: Agents with varying wealth navigate a grid, aiming to minimize inequality measured by the Gini coefficient.
+- PPO Training: A PPO agent is trained to achieve this goal, receiving sparse rewards based on Gini coefficient improvement and a large terminal reward for achieving low inequality.
+- Mesa Data Collection and Visualization: The Mesa data collector tool tracks Gini values during training, allowing for real-time visualization.
+- Visualization Script: Visualize the trained agent's behavior with Mesa's visualization tools, presenting agent movement and Gini values within the grid. 
+- Pre-trained Model: A model trained for 2 hours on 3 CPU cores is included, achieving equality (Gini < 0.1) in roughly 16-18 steps (significantly faster than a random agent which takes over 150 steps). The model behaviour as shown below is still not optimal and requires further improvements.
+
+## Future Work:
+
+- Implement Multi-Agent RL, where multiple agents with different goals optimize their individual wealth.
+
+<img src="ppo_agent.gif" width="500" height="400">
---
+++ visualization.py
@@ -0,0 +1,69 @@
+from gini_optimization import MoneyModel
+from stable_baselines3 import PPO
+import mesa
+from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.ModularVisualization import ModularServer
+
+# Modify the MoneyModel class to take actions from the RL model
+class MoneyModelRL(MoneyModel):
+    def __init__(self, N, width, height):
+        super().__init__(N, width, height)
+        self.rl_model = model = PPO.load("model/trained_model_1.zip")
+
+    def step(self):
+        # Collect data
+        self.datacollector.collect(self)
+
+        # Calculate reward
+        if self.prev_gini is None:
+            self.prev_gini = self.compute_gini()
+        new_gini = self.compute_gini()
+        if new_gini < self.prev_gini:
+            reward = (self.prev_gini - new_gini) * 20
+        else: 
+            reward = -0.05
+        self.prev_gini = new_gini
+
+        # Get observations which is the wealth of each agent and their position
+        obs = self._get_obs()
+        
+        action, _states = self.rl_model.predict(obs)
+        for i, a in enumerate(self.schedule.agents):
+            a.step(action[i])
+
+# Define the agent portrayal with different colors for different wealth levels
+def agent_portrayal(agent):
+    if agent.wealth > 10:
+        color = "purple"
+    elif agent.wealth > 7:
+        color = "red"
+    elif agent.wealth > 5:
+        color = "orange"
+    elif agent.wealth > 3:
+        color = "yellow"
+    else:
+        color = "blue"
+
+    portrayal = {"Shape": "circle",
+                 "Filled": "true",
+                 "Layer": 0,
+                 "Color": color,
+                 "r": 0.5}
+    return portrayal
+
+if __name__ == "__main__":
+
+    # Define a grid visualization
+    grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+
+    # Define a chart visualization
+    chart = ChartModule([{"Label": "Gini", "Color": "Black"}], 
+                        data_collector_name='datacollector')
+
+    # Create a modular server
+    server = ModularServer(MoneyModelRL,
+                        [grid, chart],
+                        "Money Model",
+                        {"N":10, "width":10, "height":10})
+    server.port = 8521 # The default
+    server.launch()
